2de4ffd35f857a9e5df7367424507619f52c1946
GENGE <gonzaloetjo@gmail.com>
2024-04-17 18:03:14 +0200

    fix: etst

--- flake.nix
+++ flake.nix
@@ -1,11 +1,33 @@
 {
   inputs = {
-    foundry.url = "github:shazow/foundry.nix/monthly";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    utils.url = "github:numtide/flake-utils";
+    foundry.url = "github:shazow/foundry.nix/monthly"; # Use monthly branch for permanent releases
   };
 
-  outputs = { self, foundry, ... }: {
-    overlays.default = self: super: {
-      foundry-bin = foundry.defaultPackage.${self.system};
-    };
-  };
+  outputs = { self, nixpkgs, utils, foundry }:
+    utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ foundry.overlay ];
+        };
+      in {
+
+        devShell = with pkgs; mkShell {
+          buildInputs = [
+            # From the foundry overlay
+            # Note: Can also be referenced without overlaying as: foundry.defaultPackage.${system}
+            foundry-bin
+
+            # ... any other dependencies we need
+            solc
+          ];
+
+          # Decorative prompt override so we know when we're in a dev shell
+          shellHook = ''
+            export PS1="[dev] $PS1"
+          '';
+        };
+      });
 }
